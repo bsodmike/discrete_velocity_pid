38d553a894a52199add5752b74348aaeabb3688d
Michael de Silva <michael@mwdesilva.com>
2015-05-19 01:41:07 +0530

    Initial PID simulation

---
+++ plot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for FILE in *.dat; do
+  gnuplot <<- EOF
+  set yrange [0:1200]
+  set term png
+  set output "output.png"
+  plot "${FILE}" using 1:2 with linespoints title 'output',\
+    "${FILE}" using 1:3 with lines title 'e0',\
+    "${FILE}" using 1:4 with lines title 'e1',\
+    "${FILE}" using 1:5 with lines title 'e2',\
+
+EOF
+done
+
