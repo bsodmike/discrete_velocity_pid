309908990d67a3c58acdcc2599b7b17de769ff17
Michael de Silva <michael@mwdesilva.com>
2015-05-19 22:22:44 +0530

    Add header file for PID

--- main.c
+++ main.c
@@ -5,55 +5,38 @@
 
 #include <iostream>
 #include <fstream>
+#include "PID.h"
 using namespace std;
 
-class PID {
-public:
-  float Kp;            // Loop gain parameter
-  float Ti;            // Integrator time constant
-  float Td;            // Differentiator time constant
-  float delT;          // Update time interval
+void PID::computeStep() {
+  float q0, q1, q2 = 0;
 
-  // Setpoint parameters
-  float input;         // Input from A/D converter
-  float setpt;         // Regulated level to maintain
-  float output;
-  float output_max;
-  float control0;
-  float control1;
+  e2 = e1;
+  e1 = e0;
+  // Assume the A/D reading is exactly that of the previous output
+  e0 = setpt - (output * 0.1);
+  cout << "e0: " << e0 << ", e1: " << e1 << ", e2: " << e2 << '\n';
 
-  float e0, e1, e2;
+  q0 = Kp * (1 + (delT/Ti) + (Td/delT));
+  q1 = (-1 * (Kp)) * (1 + ((2 * Td)/delT));
+  q2 = (Kp * Td) / delT;
+  cout << "q0: " << q0 << ", q1: " << q1 << ", q2: " << q2 << '\n';
 
-  void computeStep() {
-    float q0, q1, q2 = 0;
+  control1 = control0;
 
-    e2 = e1;
-    e1 = e0;
-    // Assume the A/D reading is exactly that of the previous output
-    e0 = setpt - (output * 0.1);
-    cout << "e0: " << e0 << ", e1: " << e1 << ", e2: " << e2 << '\n';
+  control0 = ((q0 * e0)+(q1 * e1)+(q2 * e2));
+  cout << "control0: " << control0 << ", control1: " << control1 << '\n';
 
-    q0 = Kp * (1 + (delT/Ti) + (Td/delT));
-    q1 = (-1 * (Kp)) * (1 + ((2 * Td)/delT));
-    q2 = (Kp * Td) / delT;
-    cout << "q0: " << q0 << ", q1: " << q1 << ", q2: " << q2 << '\n';
+  output = control0 + control1;
 
-    control1 = control0;
-
-    control0 = ((q0 * e0)+(q1 * e1)+(q2 * e2));
-    cout << "control0: " << control0 << ", control1: " << control1 << '\n';
-
-    output = control0 + control1;
-
-    // Set max saturation of output
-    if (output > output_max){
-      output = output_max;
-    } else if (output < 0) {
-      output = 0;
-    }
-    cout << "absolute control: " << output << "\n\n";
+  // Set max saturation of output
+  if (output > output_max){
+    output = output_max;
+  } else if (output < 0) {
+    output = 0;
   }
-};
+  cout << "absolute control: " << output << "\n\n";
+}
 
 int main (){
   PID pid;
---
+++ pid.h
@@ -0,0 +1,22 @@
+#include <string>
+using namespace std;
+
+class PID {
+public:
+  float Kp;            // Loop gain parameter
+  float Ti;            // Integrator time constant
+  float Td;            // Differentiator time constant
+  float delT;          // Update time interval
+
+  // Setpoint parameters
+  float input;         // Input from A/D converter
+  float setpt;         // Regulated level to maintain
+  float output;
+  float output_max;
+  float control0;
+  float control1;
+
+  float e0, e1, e2;
+
+  void computeStep();
+};
