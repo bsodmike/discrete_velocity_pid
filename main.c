1bc0cf67cde52e8c0ecf4c47ca7d304c56af1958
Michael de Silva <michael@mwdesilva.com>
2015-05-19 12:13:06 +0530

    Rather than passing a reference to the pid object to a function, setup the compute function as a method on the pid object

--- main.c
+++ main.c
@@ -7,61 +7,53 @@
 #include <fstream>
 using namespace std;
 
-class PID
-{
-  // Gain parameters
-  public:
-    float Kp;            // Loop gain parameter
-    float Ti;            // Integrator time constant
-    float Td;            // Differentiator time constant
-    float delT;          // Update time interval
+class PID {
+public:
+  float Kp;            // Loop gain parameter
+  float Ti;            // Integrator time constant
+  float Td;            // Differentiator time constant
+  float delT;          // Update time interval
 
-    // Setpoint parameters
-    float input;         // Input from A/D converter
-    float setpt;         // Regulated level to maintain
-    float output;
-    float output_max;
-    float control0;
-    float control1;
+  // Setpoint parameters
+  float input;         // Input from A/D converter
+  float setpt;         // Regulated level to maintain
+  float output;
+  float output_max;
+  float control0;
+  float control1;
 
-    float e0, e1, e2;
+  float e0, e1, e2;
 
-    // Controller state
-    float integral;      // Summation of setpoint errors
-    float deriv;         // Previous setpoint error
-};
+  void computeStep() {
+    float q0, q1, q2 = 0;
 
-void calcStep(PID *pid);
+    e2 = e1;
+    e1 = e0;
+    // Assume the A/D reading is exactly that of the previous output
+    e0 = setpt - (output * 0.1);
+    cout << "e0: " << e0 << ", e1: " << e1 << ", e2: " << e2 << '\n';
 
-void calcStep(PID *pid) {
-  float q0, q1, q2 = 0;
+    q0 = Kp * (1 + (delT/Ti) + (Td/delT));
+    q1 = (-1 * (Kp)) * (1 + ((2 * Td)/delT));
+    q2 = (Kp * Td) / delT;
+    cout << "q0: " << q0 << ", q1: " << q1 << ", q2: " << q2 << '\n';
 
-  pid->e2 = pid->e1;
-  pid->e1 = pid->e0;
-  // Assume the A/D reading is exactly that of the previous output
-  pid->e0 = pid->setpt - (pid->output * 0.1);
-  cout << "e0: " << pid->e0 << ", e1: " << pid->e1 << ", e2: " << pid->e2 << '\n';
+    control1 = control0;
 
-  q0 = pid->Kp * (1 + (pid->delT/pid->Ti) + (pid->Td/pid->delT));
-  q1 = (-1 * (pid->Kp)) * (1 + ((2 * pid->Td)/pid->delT));
-  q2 = (pid->Kp * pid->Td) / pid->delT;
-  cout << "q0: " << q0 << ", q1: " << q1 << ", q2: " << q2 << '\n';
+    control0 = ((q0 * e0)+(q1 * e1)+(q2 * e2));
+    cout << "control0: " << control0 << ", control1: " << control1 << '\n';
 
-  pid->control1 = pid->control0;
+    output = control0 + control1;
 
-  pid->control0 = ((q0 * pid->e0)+(q1 * pid->e1)+(q2 * pid->e2));
-  cout << "control0: " << pid->control0 << ", control1: " << pid->control1 << '\n';
-
-  pid->output = pid->control0 + pid->control1;
-
-  // Set max saturation of output
-  if (pid->output > pid->output_max){
-    pid->output = pid->output_max;
-  } else if (pid->output < 0) {
-    pid->output = 0;
+    // Set max saturation of output
+    if (output > output_max){
+      output = output_max;
+    } else if (output < 0) {
+      output = 0;
+    }
+    cout << "absolute control: " << output << "\n\n";
   }
-  cout << "absolute control: " << pid->output << "\n\n";
-}
+};
 
 int main (){
   PID pid;
@@ -80,7 +72,7 @@ int main (){
   plot.open("plot.dat");
 
   for(int i = 0; i < 40; i++){
-    calcStep(&pid);
+    pid.computeStep();
     plot << i << '\t' << pid.output << '\t' << pid.e0 << '\t' << pid.e1 << '\t' << pid.e2  << '\n';
   }
 
